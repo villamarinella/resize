e4822646c6bec5dc0e695bd6f79fb63922e881c6
Klaus Werner <2@sedara.net>
2017-11-09 08:22:00 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# resize
-resize all pictures in a folder to the same size
+start the program with 4 parameters:
+1. correct path with the pictures p.e. /home/kl/t1/frogs
+2. search pattern  p.e. *.jpeg
+3. resize to width p.e.  150
+4. resize to height p.e. 150
+
+python resize.py /home/kl/t1/frog *.jpeg 150 150
+
+
+If you miss something, this will help.
+
+sudo apt-get install python pip
+sudo pip install pillow
+sudo pip install resizeimage
---
+++ resize.py
@@ -0,0 +1,29 @@
+from PIL import Image
+from resizeimage import resizeimage
+import glob, os, sys
+
+if len(sys.argv) != 5:
+        print('Invalid Numbers of Arguments. Script will be terminated.')
+        print ('resize.py /home/kl/t1/frog *.jpeg 150 150')
+else:
+   	for eacharg in sys.argv:
+   		print eacharg
+   		xpath=sys.argv[1]
+   		xpattern=sys.argv[2]
+   		widht=int(sys.argv[3])
+   		height=int(sys.argv[4])
+
+def res1(file):
+	with open(file, 'r+b') as f:
+	    with Image.open(f) as image:
+	        cover = resizeimage.resize_cover(image, [widht, height])
+	        cover.save(file, image.format)
+
+
+zahl=0
+os.chdir(xpath)
+for file in glob.glob(xpattern):
+	print(file)
+	zahl=zahl+1
+	#res1(file)
+print 'Number of files: ' + str(zahl)
